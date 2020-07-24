e7a4f063c46a02f4dd3d1828436167d491a8bbaa
VBV-121 <vghadiali18@gmail.com>
2020-07-24 21:36:53 +0530

    6th project

---
+++ destini-challenge-starting/lib/story.dart
@@ -0,0 +1,12 @@
+class Story{
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({storyTitle:String, choice1:String, choice2:String}){
+        this.storyTitle=storyTitle;
+        this.choice1=choice1;
+        this.choice2=choice2;
+  }
+
+}
